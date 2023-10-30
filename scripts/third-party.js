b910fca1a4e14c73811e48a86d304ccfb205b1af
Tad Reeves <tad@arborydigital.com>
2023-10-30 15:27:33 -0400

    Adding third-party script with GA4 tracking

---
+++ scripts/third-party.js
@@ -0,0 +1,24 @@
+const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5NT7NNDZ');`;
+
+const GA_SCRIPT = `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  window.dispatchEvent(new Event('gtaginit'));
+  gtag('js', new Date());
+  gtag('config', 'G-9RTBV1LRPP');`;
+
+function createInlineScript(innerHTML, parent) {
+  const script = document.createElement('script');
+  script.type = 'text/partytown';
+  script.innerHTML = innerHTML;
+  parent.appendChild(script);
+}
+
+export default function integrateMartech() {
+  createInlineScript(GTM_SCRIPT, document.body);
+  createInlineScript(GA_SCRIPT, document.body);
+}
