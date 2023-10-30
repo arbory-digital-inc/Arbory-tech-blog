8d7fc20dc19d15d091e66b8fb033651fa9308b63
Tad Reeves <tad@arborydigital.com>
2023-10-30 16:49:04 -0400

    Adding GTM to head

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -2,7 +2,7 @@ const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5NT7NNDZ');`;
+})(window,document,'script','dataLayer','G-9RTBV1LRPP');`;
 
 const GA_SCRIPT = `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
